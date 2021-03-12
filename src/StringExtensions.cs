6c5453203a21817b61a3da95cd78ddcdb579f9e2
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-03-12 22:21:39 +0100

    Documentation for public methods

--- src/StringExtensions.cs
+++ src/StringExtensions.cs
@@ -2,23 +2,45 @@
 
 namespace Yoh.Text.Segmentation
 {
-    public static class StringExtensions
+    /// <summary>
+    /// Provides text segmentation extension methods for the string and span-related types
+    /// as specified in Annex #29 of the Unicode standard.
+    /// </summary>
+    public static class SegmentationExtensions
     {
+        /// <summary>Returns an enumeration of word boundaries for the provided string.</summary>
+        /// <param name="source">The source string for which word boundaries are enumerated.</param>
+        /// <returns>A word boundary enumerator.</returns>
         public static WordBoundaryEnumerator EnumerateWordBoundaries(this string source) =>
             new WordBoundaryEnumerator(source);
 
+        /// <summary>Returns an enumeration of word boundaries for the provided span.</summary>
+        /// <param name="source">The source span for which word boundaries are enumerated.</param>
+        /// <returns>A word boundary enumerator.</returns>
         public static WordBoundaryEnumerator EnumerateWordBoundaries(this Span<char> source) =>
             new WordBoundaryEnumerator(source);
 
+        /// <summary>Returns an enumeration of word boundaries for the provided read-only span.</summary>
+        /// <param name="source">The source read-only span for which word boundaries are enumerated.</param>
+        /// <returns>A word boundary enumerator.</returns>
         public static WordBoundaryEnumerator EnumerateWordBoundaries(this ReadOnlySpan<char> source) =>
             new WordBoundaryEnumerator(source);
 
+        /// <summary>Returns an enumeration of words for the provided string.</summary>
+        /// <param name="source">The source string for which words are enumerated.</param>
+        /// <returns>A word enumerator.</returns>
         public static WordEnumerator EnumerateWords(this string source) =>
             new WordEnumerator(source);
 
+        /// <summary>Returns an enumeration of words for the provided span.</summary>
+        /// <param name="source">The source span for which words are enumerated.</param>
+        /// <returns>A word enumerator.</returns>
         public static WordEnumerator EnumerateWords(this Span<char> source) =>
             new WordEnumerator(source);
 
+        /// <summary>Returns an enumeration of words for the provided read-only span.</summary>
+        /// <param name="source">The source read-only span for which words are enumerated.</param>
+        /// <returns>A word enumerator.</returns>
         public static WordEnumerator EnumerateWords(this ReadOnlySpan<char> source) =>
             new WordEnumerator(source);
     }
